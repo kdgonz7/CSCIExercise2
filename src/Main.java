aba59a6ed46fd2dfa6f8779f630418a19af12a59
Kai D. Gonzalez <gkai70263@gmail.com>
2026-01-27 17:50:06 -0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+void main() {
+    LinkedHashMap <Integer, Double> highTempsNextDays = new LinkedHashMap<>();
+
+    String[] dayMap = {
+            "Sunday", "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday"
+    };
+
+    highTempsNextDays.put(45, 0.95);
+    highTempsNextDays.put(29, 0.60);
+    highTempsNextDays.put(10, 0.25);
+    highTempsNextDays.put(22, 0.05);
+    highTempsNextDays.put(41, 0.0);
+    highTempsNextDays.put(28, 0.75);
+    highTempsNextDays.put(33, 0.90);
+
+    int dayCounter = 1;
+    for (Map.Entry<Integer, Double> entry : highTempsNextDays.entrySet()) {
+        if (entry.getKey() <= 32 && entry.getValue() > 0.50) {
+            System.out.println("Day " + dayMap[dayCounter] + " temp: " + entry.getKey() + ", precip: " + entry.getValue() * 100 + "%");
+        }
+
+        dayCounter++;
+    }
+}
